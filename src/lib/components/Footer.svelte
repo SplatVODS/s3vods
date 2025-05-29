22d1f4726af51c24d47f1c09e5887539b41116fd
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2025-05-29 15:23:52 -0700

    update

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<style>
+    .footer-container {
+        display: flex;
+        background-color: #d1d3d6;
+    }
+
+</style>
+
+
+<div class="footer-container">
+    
+</div>
