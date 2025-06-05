4becb0b4ddda68ea3b79f52018b78ac434598df2
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2025-06-05 16:59:30 -0700

    update

--- src/lib/data/query_state.svelte.ts
+++ src/lib/data/query_state.svelte.ts
@@ -1,8 +1,5 @@
+// If an IMPORTED variable needs to be binded or re-assigned,
+// you must use an object sytax when defining, then access the value using variable.value when binding or re-assinging
 export const currentQuery = {value: ""};
 
-// If an IMPORTED state variable needs to be binded or re-assigned,
-// you must use an object sytax when defining, then access the value using variable.value when binding or re-assinging
 export const searchQuery = $state({value: ""});
-
-// set state equal to currentQuery when clicking search button, currentQuery does not trigger a state change on its own to limit function calls
-
