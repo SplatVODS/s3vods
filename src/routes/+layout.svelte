7a2a8356b885ae31d57663e3bc5e3fe90a19c0d2
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2025-06-23 14:43:06 -0700

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,7 @@
         display: flex;
         flex-wrap: nowrap;
         flex-direction: column;
-        justify-content: flex-start; /* ??? */
+        justify-content: flex-start;
         align-items: center;
         height: 100dvh;
         width: 100%;
